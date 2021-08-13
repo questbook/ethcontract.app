1321c0a920314bdc7c5610f80fdccf090cb4ab58
Ubuntu <ubuntu@ip-172-31-24-20.ap-south-1.compute.internal>
2021-08-13 05:06:31 +0000

    ENS support

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 var createError = require('http-errors');
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var dotenv = require('dotenv');
-
+dotenv.config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mongoose = require('mongoose');
 
-dotenv.config();
 var mongoDB = process.env.MONGO_CONNECTION || 'mongodb://127.0.0.1/ethcontract.app';
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true}, err => console.log(err));
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
-var atob = require('atob');
-var btoa = require('btoa');
 var { storeAbi, retrieveAbi } = require('../models/abi');
 
+var Web3 = require('web3');
+var web3 = new Web3(process.env.INFURA);
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  try {
+    const parts = req.hostname.split(".");
+    if(parts.length === 3){
+      const name = parts[0]+".eth";
+      const address = await web3.eth.ens.getAddress(name);
+      const abi = JSON.parse((await axios.get('https://api.etherscan.io/api?module=contract&action=getabi&address='+address)).data.result);
+      const hash = await storeAbi(abi);
+
+      return res.redirect('/'+address+"?abi="+hash);	    
+    }
+  }
+  catch(e){
+	  console.log(e);
+	  return res.render('error');
+  }
   res.render('new', { title: 'Express' });
 });
 
@@ -21,10 +37,15 @@ router.post('/new', async (req, res)=> {
 
 router.get('/:address', async function(req, res) {
   const abiJson = await retrieveAbi(req.query.abi);
-  console.log(abiJson);
+  let title = req.params.address;
+  const parts = req.hostname.split(".");
+  if(parts.length === 3){
+    title += " ("+parts[0]+".eth)";
+  }
+
   const functions = abiJson.filter(a => a.type === "function");
   const variables = abiJson.filter(a => a.type !== "function" && a.type !== "constructor");
-  res.render('index',  { functions, variables, address: req.params.address, abiEncoded: req.query.abi });
+  res.render('index',  { functions, variables, address: req.params.address, abiEncoded: req.query.abi, title });
 });
 
 router.get('/:address/:function', async function(req, res) {
