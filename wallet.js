73b642ec202139979166a09baf0f86d10dd3482a
Sudham Jayanthi <workwithsudham@gmail.com>
2022-12-13 20:05:04 +0530

    save random shit

---
+++ wallet.js
@@ -0,0 +1,7 @@
+// const SignClient = require("@walletconnect/sign-client");
+// const {Web3Modal} = require("@web3modal/standalone");
+
+
+// window.SignClient = SignClient;
+// window.Web3Modal = Web3Modal;
+window["@walletconnect/sign-client"] = require("@walletconnect/sign-client");
