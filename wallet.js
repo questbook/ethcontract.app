6f08c8cccdac49aec692d7c050291c098d268841
Sudham Jayanthi <workwithsudham@gmail.com>
2022-12-15 01:15:26 +0530

    wc works. save

--- wallet.js
+++ wallet.js
@@ -1,13 +1,22 @@
-// const { SignClient } = require("@walletconnect/sign-client");
-// const { Web3Modal } = require("@web3modal/standalone");
-
-// window.SignClient = SignClient;
-// window.Web3Modal = Web3Modal;
-
-const { Web3Modal } = require("@web3modal/html");
-const { configureChains, createClient } = require("@wagmi/core");
-const { arbitrum, mainnet, polygon } = require("@wagmi/core/chains");
-const { EthereumClient, modalConnectors, walletConnectProvider } = require("@web3modal/ethereum");
+import { Web3Modal } from "@web3modal/html";
+import {
+	configureChains,
+	createClient,
+	getNetwork,
+	getAccount,
+	sendTransaction,
+	waitForTransaction,
+} from "@wagmi/core";
+import { arbitrum, mainnet, polygon } from "@wagmi/core/chains";
+import { EthereumClient, modalConnectors, walletConnectProvider } from "@web3modal/ethereum";
 
 window.w3m = { Web3Modal, EthereumClient, modalConnectors, walletConnectProvider };
-window.wagmi = { configureChains, createClient, chains: { arbitrum, mainnet, polygon } };
+window.wagmi = {
+	configureChains,
+	createClient,
+	getNetwork,
+	getAccount,
+	sendTransaction,
+	waitForTransaction,
+	chains: { arbitrum, mainnet, polygon },
+};
