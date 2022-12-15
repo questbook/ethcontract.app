8828438af458f9a1c9bfa31d7bb521fffab81346
Sudham Jayanthi <workwithsudham@gmail.com>
2022-12-15 22:56:48 +0530

    it works

---
+++ README.md
@@ -0,0 +1,16 @@
+# ethcontract.app
+## Wth is this?
+
+Ever tried using a smart contract that doesn't have frontend. Tried using etherscan? Then, you know that it sucks 🥴 
+
+Worry no more!! We got you covered with **ethcontract.app** 🎉
+
+## Features 
+- dynamic, easy-to-use interface for any contract 
+- pin functions in the ui and share the ui with your frens in a single click
+- mobile friendly (yes, you heard that right, thanks to [wallet connect and web3modal](https://github.com/WalletConnect/web3modal)) and that also means you can use it with your favorite mobile wallets like rainbow, trust etc!
+- supports all major chains like ethereum, polygon, optimism, arbitrum, goerli  
+- completely open-source source 
+
+
+## Contributors 
--- wallet.js
+++ wallet.js
@@ -2,21 +2,31 @@ import { Web3Modal } from "@web3modal/html";
 import {
 	configureChains,
 	createClient,
+	// watchAccount,
 	getNetwork,
-	getAccount,
-	sendTransaction,
-	waitForTransaction,
+	switchNetwork,
+	getContract,
+	fetchSigner,
+	readContract,
+	writeContract,
+	prepareWriteContract,
+	getAccount
 } from "@wagmi/core";
-import { arbitrum, mainnet, polygon } from "@wagmi/core/chains";
+import { arbitrum, mainnet, polygon, goerli, optimism } from "@wagmi/core/chains";
 import { EthereumClient, modalConnectors, walletConnectProvider } from "@web3modal/ethereum";
 
 window.w3m = { Web3Modal, EthereumClient, modalConnectors, walletConnectProvider };
 window.wagmi = {
 	configureChains,
 	createClient,
+	// watchAccount,
 	getNetwork,
+	switchNetwork,
+	getContract,
+	fetchSigner,
+	readContract,
+	writeContract,
+	prepareWriteContract,
 	getAccount,
-	sendTransaction,
-	waitForTransaction,
-	chains: { arbitrum, mainnet, polygon },
+	chains: { arbitrum, mainnet, polygon, goerli, optimism },
 };
